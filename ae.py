5eb01d64b60f1448d6026f71beb03fed371e1e8c
yinchuandong <yincd520@sina.com>
2016-12-09 00:22:26 +1100

    new: finish simple ae, still have bugs in conv_ae

--- ae.py
+++ ae.py
@@ -7,23 +7,40 @@ import matplotlib.pyplot as plt
 
 def main():
     # network structure
-    input_img = Input(shape=(784,))
-    encoded = Dense(32, activation='relu')(input_img)
-    decoded = Dense(784, activation='sigmoid')(encoded)
-    autoencoder = Model(input=input_img, output=decoded)
+    input_layer = Input(shape=(784,))
+    encoded = Dense(128, activation='relu')(input_layer)
+    encoded = Dense(64, activation='relu')(encoded)
+    encoded = Dense(32, activation='relu')(encoded)
+
+    decoded = Dense(64, activation='relu')(encoded)
+    decoded = Dense(128, activation='relu')(decoded)
+    decoded = Dense(784, activation='sigmoid')(decoded)
+    autoencoder = Model(input=input_layer, output=decoded)
     autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
-    encoder = Model(input=input_img, output=encoded)
-    
-    encoded_input = Input(shape=(32,))
-    decoder_layer = autoencoder.layers[-1]
-    decoder = Model(input=encoded_input, output=decoder_layer(encoded_input))
+    # build an extra model for encoder
+    # encoder = Model(input=input_layer, output=encoded)
+
+    # build an extra model for decoder
+    # encoded_input = Input(shape=(32,))
+    # decoder_layer = autoencoder.layers[-3](encoded_input)
+    # decoder_layer = autoencoder.layers[-2](decoder_layer)
+    # decoder_layer = autoencoder.layers[-1](decoder_layer)
+    # decoder = Model(input=encoded_input, output=decoder_layer)
 
     (x_train, _), (x_test, _) = mnist.load_data()
     x_train = x_train.astype('float32') / 255.
     x_test = x_test.astype('float32') / 255.
     x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
     x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+    noise_factor = 0.5
+    x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
+    x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
+
+    x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+    x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+
     print x_train.shape
     print x_test.shape
 
@@ -31,17 +48,18 @@ def main():
                     nb_epoch=50,
                     batch_size=256,
                     shuffle=True,
-                    validation_data=(x_test, x_test))
+                    validation_data=(x_test_noisy, x_test))
 
-    encoded_imgs = encoder.predict(x_test)
-    decoded_imgs = decoder.predict(encoded_imgs)
+    # encoded_imgs = encoder.predict(x_test)
+    # decoded_imgs = decoder.predict(encoded_imgs)
+    decoded_imgs = autoencoder.predict(x_test)
 
     n = 10 
     plt.figure(figsize=(20, 4))
     for i in range(n):
         # display original
         ax = plt.subplot(2, n, i + 1)
-        plt.imshow(x_test[i].reshape(28, 28))
+        plt.imshow(x_test_noisy[i].reshape(28, 28))
         plt.gray()
         ax.get_xaxis().set_visible(False)
         ax.get_yaxis().set_visible(False)
--- conv_ae.py
+++ conv_ae.py
@@ -1,60 +1,69 @@
-from keras.layers import Input, Dense, Convolution2D, MaxPooling2D, UpSampling2D
+from keras.layers import Input, Convolution2D, Deconvolution2D, MaxPooling2D, UpSampling2D
 from keras.models import Model
 from keras.datasets import mnist
+from keras.callbacks import TensorBoard
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-# load data
-(x_train, _), (x_test, _) = mnist.load_data()
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-x_train = np.reshape(x_train, (len(x_train), 1, 28, 28))
-x_test = np.reshape(x_test, (len(x_test), 1, 28, 28))
+def main():
+    input_img = Input(shape=(1, 28, 28))
 
-noise_factor = 0.5
-x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
-x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
-x_train_noisy = np.clip(x_train_noisy, 0., 1.)
-x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+    x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(input_img)
+    x = MaxPooling2D((2, 2), border_mode='same')(x)
+    x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(x)
+    encoded = MaxPooling2D((2, 2), border_mode='same')(x)
 
-n = 10
-plt.figure(figsize=(20, 2))
-for i in range(n):
-    ax = plt.subplot(1, n, i + 1)
-    plt.imshow(x_test_noisy[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
+    # at this point the representation is (32, 7, 7)
+
+    x = Deconvolution2D(32, 3, 3, activation='relu', border_mode='same')(encoded)
+    x = UpSampling2D((2, 2))(x)
+    x = Deconvolution2D(32, 3, 3, activation='relu', border_mode='same')(x)
+    x = UpSampling2D((2, 2))(x)
+    decoded = Deconvolution2D(1, 3, 3, activation='sigmoid', border_mode='same')(x)
+
+    autoencoder = Model(input_img, decoded)
+    autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+    # train
+    (x_train, _), (x_test, _) = mnist.load_data()
+    x_train = x_train.astype('float32') / 255.
+    x_test = x_test.astype('float32') / 255.
+    x_train = np.reshape(x_train, (len(x_train), 1, 28, 28))
+    x_test = np.reshape(x_test, (len(x_test), 1, 28, 28))
+    print x_train.shape
+    print x_test.shape
+
+    autoencoder.fit(x_train, x_train,
+                    nb_epoch=50,
+                    batch_size=128,
+                    shuffle=True,
+                    validation_data=(x_test, x_test),
+                    callbacks=[TensorBoard(log_dir='./tmp')])
+
+    # encoded_imgs = encoder.predict(x_test)
+    # decoded_imgs = decoder.predict(encoded_imgs)
+    decoded_imgs = autoencoder.predict(x_test)
+
+    n = 10 
+    plt.figure(figsize=(20, 4))
+    for i in range(n):
+        # display original
+        ax = plt.subplot(2, n, i + 1)
+        plt.imshow(x_test[i].reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        # display reconstruction
+        ax = plt.subplot(2, n, i + 1 + n)
+        plt.imshow(decoded_imgs[i].reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.show()
+    return
 
 
-input_img = Input(shape=(1, 28, 28))
-
-x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(input_img)
-x = MaxPooling2D((2, 2), border_mode='same')(x)
-x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(x)
-encoded = MaxPooling2D((2, 2), border_mode='same')(x)
-
-# at this point the representation is (32, 7, 7)
-
-x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(encoded)
-x = UpSampling2D((2, 2))(x)
-x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(x)
-x = UpSampling2D((2, 2))(x)
-decoded = Convolution2D(1, 3, 3, activation='sigmoid', border_mode='same')(x)
-
-autoencoder = Model(input_img, decoded)
-autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
-
-
-# autoencoder.fit(x_train_noisy, x_train,
-#                 nb_epoch=100,
-#                 batch_size=128,
-#                 shuffle=True,
-#                 validation_data=(x_test_noisy, x_test),
-#                 callbacks=[TensorBoard(log_dir='/tmp/tb', histogram_freq=0, write_graph=False)])
-
-
-
-
+if __name__ == '__main__':
+    main()
